65603094b49530b4459584da4f0ba4935cfd7a98
Alberto Martínez Quesada <betomqcr@gmail.com>
2024-06-21 13:37:47 -0600

    Implementar metodo nuevo

--- Controllers/ConversationsController.cs
+++ Controllers/ConversationsController.cs
@@ -4,6 +4,8 @@ using InfintyHibotPlt.Negocio.Class;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System.Net;
 
 namespace InfintyHibotPlt.Controllers
@@ -21,11 +23,98 @@ namespace InfintyHibotPlt.Controllers
             this.Environment = environment;
         }
 
-        
+        [HttpPost]
+        [Route("hibot")]
+        public IActionResult HandleWebhook([FromBody] dynamic payload)
+        {
+            try
+            {
+                var converter = new ExpandoObjectConverter();
+                string jsonString = JsonConvert.SerializeObject(payload, converter);
+
+                Request request = InfintyHibotPlt.Datos.Hibot.Models.ConversationsFolder.Request.FromJson(jsonString);
+                if (request != null)
+                {
+                    Conversation conversation = new Conversation
+                    {
+                        contactName = request.Conversations[0].Contacts[0].Fields.Name,
+                        contactPhoneWA = request.Conversations[0].Contacts[0].Account,
+                        agente = request.Conversations[0].Agent.Name,
+                        agenteEmail = request.Conversations[0].Agent.Email,
+                        typing = request.Conversations[0].Typing,
+                        estado = request.Conversations[0].Type,
+                        idHibotConversation = request.Conversations[0].Id,
+                        closed = request.Conversations[0].Closed,
+                        create = request.Conversations[0].Created,
+                        assigend = request.Conversations[0].Assigned
+
+                    };
+                    context.Conversations.Add(conversation);
+                    context.SaveChanges();
+                    long idConvesartion = context.Conversations.Where(x => x.idHibotConversation.Equals(conversation.idHibotConversation)).First().idConversation;
+                    foreach (InMessage temp in request.Conversations[0].Messages)
+                    {
+                        Messages messages = new Messages
+                        {
+                            ConversationidConversation = idConvesartion,
+                            content = temp.Content,
+                            personContent = temp.From,
+
+                            idHibotMessages = temp.Id
+                        };
+
+                        if (temp.media != null)
+                        {
+                            messages.media = temp.media.ToString();
+                            messages.mediaType = temp.mediaType;
+                        }
+                        if (temp.Created != null)
+                            messages.created = temp.Created;
+                        if (temp.Createdpub != null && temp.Created == null)
+                            messages.created = temp.Createdpub;
+
+                        context.Messages.Add(messages);
+                        context.SaveChanges();
+
+                        if (temp.media != null)
+                        {
+                            long idMessage = context.Messages.Where(x => x.idHibotMessages.Equals(temp.Id)).FirstOrDefault().idMessages;
+
+                        }
+                    }
+                    Bitacora bitacora = new Bitacora
+                    {
+                        idConversation = idConvesartion,
+                        Estado = request.Conversations[0].Typing,
+                    };
+                    bitacora.jsonEntrada = Serialize.ToJson(request);
+                    context.Bitacora.Add(bitacora);
+                    context.SaveChanges();
+
+                    return Ok();
+                }
+                else
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception ex) 
+            {
+                ErroresBitacora errorBitacora = new ErroresBitacora();
+                errorBitacora.menssageError = ex.Message;
+                errorBitacora.Fecha = DateTime.Now;
+                context.ErroresBitacora.Add(errorBitacora);
+                context.SaveChanges();
+
+                return Ok();
+            }
+        }
+     
+
 
         [HttpPost]
-        [Route("Recibir")]
-        public async Task<IActionResult> Recibir(Request request)
+        [Route("recibir")]
+        public async Task<IActionResult> Recibir([FromBody] Request request)
         {
             try
             {
@@ -87,11 +176,11 @@ namespace InfintyHibotPlt.Controllers
                     context.Bitacora.Add(bitacora);
                     context.SaveChanges();
 
-                    return Ok("Recibido");
+                    return Ok();
                 }
                 else
                 {
-                    return Ok("Recibido");
+                    return Ok();
                 }
             }
             catch (Exception ex)
@@ -107,86 +196,7 @@ namespace InfintyHibotPlt.Controllers
             
         }
 
-        [HttpPost]
-        [Route("RecibirLista")]
-        public async Task<IActionResult> RecibirLista(List<Request> request)
-        {
-            try
-            {
-                if (request != null)
-                {
-                    foreach (Request r in request)
-                    {
-                        Conversation conversation = new Conversation
-                        {
-                            contactName = r.Conversations[0].Contacts[0].Fields.Name,
-                            contactPhoneWA = r.Conversations[0].Contacts[0].Account,
-                            agente = r.Conversations[0].Agent.Name,
-                            agenteEmail = r.Conversations[0].Agent.Email,
-                            typing = r.Conversations[0].Typing,
-                            estado = r.Conversations[0].Type,
-                            idHibotConversation = r.Conversations[0].Id,
-                            closed = r.Conversations[0].Closed,
-                            create = r.Conversations[0].Created,
-                            assigend = r.Conversations[0].Assigned
-
-                        };
-                        context.Conversations.Add(conversation);
-                        context.SaveChanges();
-                        long idConvesartion = context.Conversations.Where(x => x.idHibotConversation.Equals(conversation.idHibotConversation)).First().idConversation;
-                        foreach (InMessage temp in r.Conversations[0].Messages)
-                        {
-                            Messages messages = new Messages
-                            {
-                                ConversationidConversation = idConvesartion,
-                                content = temp.Content,
-                                personContent = temp.From,
-
-                                idHibotMessages = temp.Id
-                            };
-
-                            if (temp.media != null)
-                            {
-                                messages.media = temp.media.ToString();
-                                messages.mediaType = temp.mediaType;
-                            }
-                            if (temp.Created != null)
-                                messages.created = temp.Created;
-                            if (temp.Createdpub != null && temp.Created == null)
-                                messages.created = temp.Createdpub;
-
-                            context.Messages.Add(messages);
-                            context.SaveChanges();
-                        }
-                        Bitacora bitacora = new Bitacora
-                        {
-                            idConversation = idConvesartion,
-                            Estado = r.Conversations[0].Typing,
-                        };
-                        bitacora.jsonEntrada = Serialize.ToJson(r);
-                        context.Bitacora.Add(bitacora);
-                        context.SaveChanges();
-                    }
-
-                    return Ok("Recibido");
-                }
-                else
-                {
-                    return Ok("Recibido");
-                }
-            }
-            catch (Exception ex)
-            {
-                ErroresBitacora errorBitacora = new ErroresBitacora();
-                errorBitacora.menssageError = ex.Message;
-                errorBitacora.Fecha = DateTime.Now;
-                context.ErroresBitacora.Add(errorBitacora);
-                context.SaveChanges();
-
-                return Ok(ex);
-            }
-
-        }
+        
 
         [HttpPost]
         [Route("ObtenerScript")]
